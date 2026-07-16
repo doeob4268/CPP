975690f382ec26939a224959f713c00fa5637af9
rei1102 <37990020+reiNatsu@users.noreply.github.com>
2026-07-16 22:05:32 +0900

    ddfddfdf

--- CPP/Parts/Part05.cpp
+++ CPP/Parts/Part05.cpp
@@ -8,5 +8,75 @@ Part05::Part05()
 
 void Part05::ArrayListClass()
 {
+	using namespace Part05_ArrayList;
 
+	ArrayList arr1;
+
+	for (int i = 0; i < 10; ++i)
+		arr1.push_back(i + 1);
+
+	ArrayList arr2(arr1);
+	ArrayList arr3;
+
+	arr3 = arr1;
+
+	cout << "===== 배열 리스트 1 원소 =====\n";
+	PrintArray(arr1.data(), arr1.size());
+
+	cout << "===== 배열 리스트 2 원소 =====\n";
+	PrintArray(arr2.data(), arr2.size());
+
+	cout << "===== 배열 리스트 3 원소 =====\n";
+	PrintArray(arr3.data(), arr3.size());
+}
+
+void Part05::PrintArray(int* arr, size_t size)
+{
+	for (size_t i = 0; i < size - 1; ++i)
+		cout << arr[i] << ", ";
+
+	cout << arr[size - 1];
+
+	cout << '\n';
+}
+
+namespace Part05_ArrayList
+{
+	ArrayList& ArrayList::operator=(const ArrayList& other)
+	{
+		// TODO: 여기에 return 문을 삽입합니다.
+		
+		ArrayList push_back(10); // 초기 용량을 10으로 설정
+		ArrayList arr1(10); 
+		for (int i = 0; i < 10; ++i) 
+			arr1.push_back(i + 1); 
+
+		return *this; 
+
+	}
+
+	ArrayList& ArrayList::operator=(ArrayList&& other) noexcept
+	{
+		// TODO: 여기에 return 문을 삽입합니다.
+	}
+
+	ArrayList::ArrayList(size_t capacity)
+	{
+	}
+
+	ArrayList::ArrayList(const ArrayList& other)
+	{
+	}
+
+	ArrayList::ArrayList(ArrayList&& other) noexcept
+	{
+	}
+
+	ArrayList::~ArrayList()
+	{
+	}
+
+	void ArrayList::push_back(int value)
+	{
+	}
 }
--- CPP/Parts/Part05.h
+++ CPP/Parts/Part05.h
@@ -5,19 +5,45 @@ namespace Part05_ArrayList
 	class ArrayList
 	{
 	public:
-		int Arr[10];
-		int ProportionArr[];
+		// ===== 연산자 오버로딩 =====
+		// 복사 대입
+		ArrayList& operator=(const ArrayList& other);
+		// 이동 대입
+		ArrayList& operator=(ArrayList&& other) noexcept;
 
+		// ===== 자원 관리 =====
+		// 생성자
+		explicit ArrayList(size_t capacity = 0);
+		// 복사 생성자
+		ArrayList(const ArrayList& other);
+		// 이동 생성자
+		ArrayList(ArrayList&& other) noexcept;
+		// 소멸자
+		~ArrayList();
+
+		// ===== 컨테이너 제어 =====
+		int* data() {}
+		const int* data() const {}
+
+		size_t capacity() const {}
+		size_t size() const {}
+		// 데이터 추가
+		void push_back(int value);
 
 	private:
-
-
+		size_t cap = 0;
+		size_t numElements = 0;
+		int* arr = nullptr;
 	};
 }
+
 class Part05
 {
 public:
 	Part05();
 
 	void ArrayListClass();
+
+private:
+	void PrintArray(int* arr, size_t size);
 };
