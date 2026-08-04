4a003401ddfe4bc56e40dd153db24d1de3cea319
doeun <haibakuro@gmail.com>
2026-08-04 21:49:58 +0900

    // 데이터 추가 함수 (AddData) // 데이터 삭제 함수 (RemoveData) // 데이터 존재 확인 함수 (Contains) // 저장된 데이터 개수 반환 함수 (Size) // 비었는지 확인하는 함수 (Empty) // 싱글턴 객체는 유지하면서 내부 데이터만 제거하는 함수 (Clear)

--- CPP/Part06.cpp
+++ CPP/Part06.cpp
@@ -3,9 +3,12 @@
 
 int Part06_StaticMember::Widget::staticValue = 0;//초기화 할떄 0이라는 값이 들어감
 
+Part06_Singleton::DataManager* Part06_Singleton::DataManager::instance = nullptr;
+
 Part06::Part06()
 {
-	StaticMember();
+	//StaticMember();
+	Singleton();
 }
 
 void Part06::StaticMember()//멤버변수는 객체에 따라 저장된다
@@ -28,5 +31,54 @@ void Part06::StaticMember()//멤버변수는 객체에 따라 저장된다
 
 void Part06::Singleton()
 {
+	using namespace Part06_Singleton;
+
+	//DataManager::Get()->Create();//객체를 만들기도 전에 먼저 Get을 하면 nullptr이 나오는데 Func호출하면 오류가 나서 문제가 생김
+	DataManager* manager = DataManager::Get();
+	assert(manager == DataManager::Get());
+
+	// 만든 기능들을 전부 1회 이상 활용해보는 예제 직접 만들어보기
+
+	DataManager::Destroy();
+	DataManager::AddData();
+}
+
+namespace Part06_Singleton
+{
+	DataManager* DataManager::Get()
+	{
+		if (instance == nullptr)
+			instance = new DataManager();//이런 기법을 게으른 초기화 라고 한다
+										//장점 : 필요하기 전까지 안 만들수있다 (메모리 이점)
+										//단점 : 런타임에 동적할당 하는거기 때문에 (로딩창 없이) 좋지 않다 
+		return instance;
+	}
+
+	void DataManager::Destroy() noexcept
+	{
+		SAFE_DELETE(instance);
+	}
+	void DataManager::AddData() noexcept
+	{
+		cout << "데이터 추가하기";
+		int NewData;
+		cin >> new Data
+	}
+	void DataManager::RemoveData() noexcept
+	{
+		cout << "데이터 삭제하기";
+
+		SAFE_DELETE();
+	}
+
+	int DataManager::FindIndex(const string& id) const noexcept
+	{
+		for (size_t i = 0; i < numDatas; ++i)
+		{
+			if (datas[i].id == id)
+				return static_cast<int>(i);
+		}
+		return -1;
+	}
 
 }
--- CPP/Part06.h
+++ CPP/Part06.h
@@ -19,9 +19,59 @@ namespace Part06_StaticMember
 	};
 }
 
-namespace Part06_Singleton
+namespace Part06_Singleton//유일한 객체 하나만 만드는 것, 게임업계에서 많이 쓰임 <- 싱글턴
 {
+	struct Data
+	{
+		string id;
 
+		int value = 0;
+		float fltValue = 0.0f;
+	};
+
+	class DataManager final //data를 가지고 관리하는 클래스
+	{
+	public:
+		//static void Create()
+		//{
+		//	if (instance == nullptr)
+		//		instance = new DataManager();
+		//}
+		static DataManager* Get();
+
+		static void Destroy() noexcept;
+		static void AddData()noexcept;
+		static void RemoveData()noexcept;
+
+		// 데이터 추가 함수 (AddData)
+		// 데이터 삭제 함수 (RemoveData)
+		// 데이터 존재 확인 함수 (Contains)
+		// 저장된 데이터 개수 반환 함수 (Size)
+		// 비었는지 확인하는 함수 (Empty)
+		// 싱글턴 객체는 유지하면서 내부 데이터만 제거하는 함수 (Clear)
+
+	private:
+		// ID로 배열 인덱스 찾기
+		int FindIndex(const string& id) const noexcept;
+
+
+		//객체 생성과 복사 제한
+		DataManager() = default;//자동으로 생성자가 public에 생성되는데 private에 먼저 만들어 둔다
+		~DataManager() = default;//외부에서 소멸자도 막기 위해 내부에서 만든다, 생성이랑 소멸은 해야해서 내부에서 감춘다
+
+		DataManager(const DataManager&) = delete;//이 함수를 사용하면 삭제가 되어 컴파일러에서 막는다
+		DataManager& operator=(const DataManager&) = delete;
+
+		DataManager(DataManager&&) = delete;
+		DataManager& operator=(DataManager&&) = delete;
+		// 멤버변수
+		static constexpr size_t MAX_DATA_COUNT = 100;//스태틱 붙으면 정적멤버, 선언할때 초기화가 되야하기 때문에 여기서 초기화가 된다
+
+		Data datas[MAX_DATA_COUNT]{};
+		size_t numDatas = 0;
+		
+		static DataManager* instance;//포인터는 그냥 주소값이기 때문에 가능함
+	};
 }
 
 class Part06
